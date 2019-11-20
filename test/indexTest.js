f1ebde7426b6c7fcf83366404bb8af6467d043a8
srijakumar <srijakumar7@gmail.com>
2019-11-20 18:21:58 -0500

    Done.

--- test/indexTest.js
+++ test/indexTest.js
@@ -3,7 +3,12 @@ const jsdom = require('mocha-jsdom')
 const path = require('path')
 const babel = require('babel-core');
 
+
+
 const js = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+
+
+
 
 describe('index.js', function () {
   describe('companyName', function () {
